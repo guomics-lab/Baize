025e8b600cc85582de13a330d5ce0d105e04b726
niezongxiang <niezongxiang@womics.com>
2025-04-27 10:52:46 +0800

    New code

--- web_main.py
+++ web_main.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 from src.baize.baize_router import baize_bp
 
-
-
 app = Flask(__name__)
+
 
 class CaseInsensitiveMiddleware:
     def __init__(self, app):
@@ -17,11 +16,10 @@ class CaseInsensitiveMiddleware:
 
         return self.app(environ, start_response)
 
+
 app.wsgi_app = CaseInsensitiveMiddleware(app.wsgi_app)
 
 app.register_blueprint(baize_bp, url_prefix='/baize')
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=9997, host='0.0.0.0')
-
